6de23b70445915f8faadef2d5116c683278dde46
Boris Le Meec <boris.le-meec@epitech.eu>
2018-10-11 16:21:10 +0200

    best usage

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,35 +1,35 @@
 ﻿using System;
-using System.IO;
-using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Assignment3
 {
-    class Program
+    internal static class Program
     {
-        static TcpListener server = null;
-        static TcpClient client = null;
-        static int counter = 0;
-        static String data = null;
-        // Buffer for reading data
-        static Byte[] bytes = new Byte[256];
+        private static TcpListener _server;
+        private static TcpClient _client;
+        private static int _counter;
 
-        static void Main(string[] args)
+        private static string _data;
+        // Buffer for reading data
+        private static readonly byte[] Bytes = new byte[256];
+
+        private static void Main(string[] args)
         {
 
             try
             {
                 // Set the TcpListener on port 5000.
-                Int32 port = 5000;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                const int port = 5000;
+                var localAddr = IPAddress.Parse("127.0.0.1");
 
                 // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
+                _server = new TcpListener(localAddr, port);
 
                 // Start listening for client requests.
-                server.Start();
+                _server.Start();
 
                 // Enter the listening loop.
                 while (true)
@@ -38,10 +38,10 @@ namespace Assignment3
 
                     // Perform a blocking call to accept requests.
                     // You could also user server.AcceptSocket() here.
-                    client = server.AcceptTcpClient();
-                    counter += 1;
-                    Console.WriteLine("Connected!" + "Client No:" + Convert.ToString(counter));
-                    Thread connect = new Thread(HandleClient);
+                    _client = _server.AcceptTcpClient();
+                    _counter += 1;
+                    Console.WriteLine("Connected!" + "Client No:" + Convert.ToString(_counter));
+                    var connect = new Thread(HandleClient);
                     connect.Start();
 
                 }
@@ -53,7 +53,7 @@ namespace Assignment3
             finally
             {
                 // Stop listening for new clients.
-                server.Stop();
+                _server.Stop();
             }
 
 
@@ -61,33 +61,33 @@ namespace Assignment3
             Console.Read();
         }
 
-        static void HandleClient()
+        private static void HandleClient()
         {
-            data = null;
+            _data = null;
             // Get a stream object for reading and writing
-            NetworkStream stream = client.GetStream();
+            var stream = _client.GetStream();
 
             int i;
 
             // Loop to receive all the data sent by the client.
-            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            while ((i = stream.Read(Bytes, 0, Bytes.Length)) != 0)
             {
                 // Translate data bytes to a ASCII string.
-                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                Console.WriteLine("Received: {0}", data);
+                _data = Encoding.ASCII.GetString(Bytes, 0, i);
+                Console.WriteLine("Received: {0}", _data);
 
                 // Process the data sent by the client.
-                data = data.ToUpper();
+                _data = _data.ToUpper();
 
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                var msg = Encoding.ASCII.GetBytes(_data);
 
                 // Send back a response.
                 stream.Write(msg, 0, msg.Length);
-                Console.WriteLine("Sent: {0}", data);
+                Console.WriteLine("Sent: {0}", _data);
             }
 
             // Shutdown and end connection
-            client.Close();
+            _client.Close();
         }
     }
 }
---
+++ Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            try 
+            {
+                if (args.Length < 2)
+                    return;
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                const int port = 5000;
+                var client = new TcpClient(args[0], port);
+    
+                // Translate the passed message into ASCII and store it as a Byte array.
+                var data = System.Text.Encoding.ASCII.GetBytes(args[1]);         
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+    
+                var stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", args[1]);         
+
+                // Receive the TcpServer.response.
+    
+                // Buffer to store the response bytes.
+                data = new byte[256];
+
+                // String to store the response ASCII representation.
+
+                // Read the first batch of the TcpServer response bytes.
+                var bytes = stream.Read(data, 0, data.Length);
+                var responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);         
+
+                // Close everything.
+                stream.Close();         
+                client.Close();         
+            } 
+            catch (ArgumentNullException e) 
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            } 
+            catch (SocketException e) 
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+    
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
