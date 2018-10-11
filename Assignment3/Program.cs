e31da0b0b4edab92c6ccd4915a2710b819f48165
Ana Pecini <anna.pecini@yahoo.com>
2018-10-11 16:04:34 +0200

    Multithread clients

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -9,9 +9,16 @@ namespace Assignment3
 {
     class Program
     {
+        static TcpListener server = null;
+        static TcpClient client = null;
+        static int counter = 0;
+        static String data = null;
+        // Buffer for reading data
+        static Byte[] bytes = new Byte[256];
+
         static void Main(string[] args)
         {
-            TcpListener server = null;
+            
             try
             {
                 // Set the TcpListener on port 5000.
@@ -24,10 +31,6 @@ namespace Assignment3
                 // Start listening for client requests.
                 server.Start();
 
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
                 // Enter the listening loop.
                 while (true)
                 {
@@ -35,35 +38,12 @@ namespace Assignment3
 
                     // Perform a blocking call to accept requests.
                     // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
+                    client = server.AcceptTcpClient();
+                    counter += 1;
+                    Console.WriteLine("Connected!" + "Client No:" + Convert.ToString(counter));
+                    Thread connect = new Thread(HandleClient);
+                    connect.Start();            
 
-                    data = null;
-
-                    // Get a stream object for reading and writing
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Loop to receive all the data sent by the client.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Translate data bytes to a ASCII string.
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
-
-                        // Process the data sent by the client.
-                        data = data.ToUpper();
-
-                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                        // Send back a response.
-                        stream.Write(msg, 0, msg.Length);
-                        Console.WriteLine("Sent: {0}", data);
-                    }
-
-                    // Shutdown and end connection
-                    client.Close();
                 }
             }
             catch (SocketException e)
@@ -79,6 +59,35 @@ namespace Assignment3
 
             Console.WriteLine("\nHit enter to continue...");
             Console.Read();
+        }
+
+        static void HandleClient()
+        {
+            data = null;
+            // Get a stream object for reading and writing
+            NetworkStream stream = client.GetStream();
+
+            int i;
+
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                // Process the data sent by the client.
+                data = data.ToUpper();
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                // Send back a response.
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("Sent: {0}", data);
+            }
+
+            // Shutdown and end connection
+            client.Close();
         }
     }
 }
