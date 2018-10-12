e4e0c18ba0ddd1ee838f6328ce08c0474c8dede0
William Herrgott <herrgott.william@gmail.com>
2018-10-12 15:44:44 +0200

    Curly again

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -164,19 +164,16 @@ namespace Assignment3 {
             if (IsPathOk(r, ref resp) == 1) {
                 return;
             }
-
-            var id = -1;
-            try
-            {
+            
+            int id;
+            try {
                 id = Convert.ToInt32(r.Path.Substring(12));
             }
-            catch (FormatException)
-            {
+            catch (FormatException) {
                 resp.Status = "4 Bad request";
                 return;
             }
-            for (var i = 0; i < Globals.Db.Count; i++)
-            {
+            for (var i = 0; i < Globals.Db.Count; i++) {
                 if (Globals.Db[i].Uid != id) continue;
                 Globals.Db.RemoveAt(i);
                 resp.Status = "1 Ok";
